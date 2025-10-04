6c9e53b0ecc1109e19c5a3642df991498f156274
Brenda Luana <brenda.castro@estudante.iftm.edu.br>
2025-10-03 23:27:11 -0300

    Add files via upload

---
+++ SegundaAvaliacao/ex02/bootstrap-main/src/main/java/com/professorangoti/bootstrap/HomeController.java
@@ -0,0 +1,17 @@
+package com.professorangoti.bootstrap;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+ @Controller
+public class HomeController {
+   
+    @GetMapping("/home.html")
+    public String home (Model model)
+    {
+    model.addAttribute("");
+    return "home"; // 
+
+    }
+}
